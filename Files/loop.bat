94b24d34bb753e0f8e4a1987b129b7bf21986945
DWS1080 <twin1218@outlook.jp>
2021-07-03 20:59:18 +0900

    Update loop.bat

--- Files/loop.bat
+++ Files/loop.bat
@@ -1,5 +1,5 @@
 @echo off
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "NGROK Services is not Running. One NGROK FREE Account/ 1 Tunnel, If u want Run more VM, Update new NGROK_AUTH_TOKEN into Settings> Secrets> Repository secrets then run workflow again. Your current workflow end shortly. " & ping 127.0.0.1 >Nul & ping 127.0.0.1 >Nul & exit
+tasklist | find /i "ngrok.exe" >Nul && goto check || echo "NGROKサービスが起動していません。1つのNGROK無料アカウント/ 1 トンネルをもっとVMを実行したい場合は、新しいNGROK_AUTH_TOKENを設定>Secrets>Repository Secretsにアップデートしてから、再度ワークフローを実行してください。現在のワークフローはまもなく終了します。" & ping 127.0.0.1 >Nul & ping 127.0.0.1 >Nul & exit
 :check
 ping 127.0.0.1 > nul
 cls
